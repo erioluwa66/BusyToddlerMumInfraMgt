2ec85555ef086edcc54aa23bdd8ee4a1609f1bad
erioluwa66 <erioluwa66@gmail.com>
2024-04-18 13:05:02 -0400

    string added

--- variables.tf
+++ variables.tf
@@ -5,5 +5,6 @@ variable "aws_region" {
 
 variable "oidc_thumbprint" {
   description = "The SHA-1 thumbprint for the OIDC provider"
-  type = "959cb2b52b4ad201a593847abca32ff48f838c2e"
+  type = string
+  default =   "959cb2b52b4ad201a593847abca32ff48f838c2e"
 }
