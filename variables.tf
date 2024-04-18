7cc097de9499aa8fabeb863ea5e3ae69d6226292
erioluwa66 <erioluwa66@gmail.com>
2024-04-18 14:05:09 -0400

    data added

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  default = "us-east-2"
+  default     = "us-east-2"
 }
 
 variable "oidc_thumbprint" {
   description = "The SHA-1 thumbprint for the OIDC provider"
-  type = string
-  default =   "959cb2b52b4ad201a593847abca32ff48f838c2e"
+  type        = string
+  default     = "959cb2b52b4ad201a593847abca32ff48f838c2e"
 }
